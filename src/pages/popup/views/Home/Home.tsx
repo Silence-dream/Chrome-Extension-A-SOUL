e3d652a1fddce4be1f2b3e2cf1dc31fe1ac25e41
silence <1428482231@qq.com>
2022-04-11 16:15:00 +0800

    feat: 综合页面渲染

--- src/pages/popup/views/Home/Home.tsx
+++ src/pages/popup/views/Home/Home.tsx
@@ -1,7 +1,12 @@
+import './Home.module.scss';
+
+import VideoReatedList from '@/pages/popup/components/VideoRelatedList/VideoReatedList';
 export default function Home() {
   return (
     <>
-      <div id="home">12312</div>
+      <div id="home">
+        <VideoReatedList />
+      </div>
     </>
   );
 }
